a89dfa0644c1bdd3bdfa1e0d4325e9704aaa9ad7
SinkLineP <popovartem505@gmail.com>
2019-12-08 20:23:04 +0200

    add "if_else" from "visit"

--- app.rb
+++ app.rb
@@ -7,10 +7,10 @@ require 'sinatra/activerecord'
 set :database, {adapter: "sqlite3", database: "barber.db"}
 
 class Clients < ActiveRecord::Base
-	validates :username, presence: true
-	validates :phone, presence: true
-	validates :datetime, presence: true
-	validates :color, presence: true
+	# validates :username, presence: true
+	# validates :phone, presence: true
+	# validates :datetime, presence: true
+	# validates :color, presence: true
 end
 
 class Barbers < ActiveRecord::Base
@@ -36,11 +36,12 @@ end
 post '/visit' do 
 
 	c = Clients.new params[:client]
-    c.save
-
-    
-	erb "<h2>Спасибо вы записались!!</h2>"
-	
+	c.save
+    if c.save
+		erb "<h2>Спасибо вы записались!!</h2>"
+	else
+		erb "<h2>Ошибка</h2>"
+	end
 end
 
 get '/contacts' do
